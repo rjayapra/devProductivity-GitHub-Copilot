609e042fb05cfa3f7dd9e6f180f289e9ed499c54
Radhika Jayaprakash <85903942+rjayapra@users.noreply.github.com>
2024-09-13 13:52:44 -0400

    Merge

--- src/Application/src/RazorPagesTestSample/Data/Message.cs
+++ src/Application/src/RazorPagesTestSample/Data/Message.cs
@@ -2,23 +2,20 @@ using System.ComponentModel.DataAnnotations;
 
 namespace RazorPagesTestSample.Data
 {
-    #region snippet1
-    /// <summary>
-    /// Represents a message with an ID and text content.
-    /// </summary>
-    /// /// /// public class Message
+     #region snippet1
+    public class Message
     {
+        public int Id { get; set; }
+    
         /// <summary>
         /// Gets or sets the unique identifier for the message.
         /// </summary>
-        /// /// /// public int Id { get; set; }
+     
 
         /// <summary>
         /// Gets or sets the text content of the message.
         /// </summary>
-        /// <remarks>
-        /// The text content is required and has a maximum length of 250 characters.
-        /// </remarks>
+        
         [Required]
         [DataType(DataType.Text)]
         [StringLength(250, ErrorMessage = "There's a 250 character limit on messages. Please shorten your message.")]
